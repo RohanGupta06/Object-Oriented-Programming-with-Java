a6074bdc6e8c57c1fd67553340354cf5af800caf
Rohan gupta <rohange@outlook.com>
2020-04-16 23:07:33 +0530

    auto boxing

---
+++ Autoboxing.java
@@ -0,0 +1,15 @@
+package Autoboxing;
+import java.util.ArrayList;
+
+public class Autoboxing {
+
+	public static void main(String[] args) {
+		char ch = 'a';
+		Character a = ch;
+		ArrayList<Integer> arrayList = new ArrayList<Integer>();
+		arrayList.add(25);
+		System.out.println(arrayList.get(0));
+
+	}
+
+}
